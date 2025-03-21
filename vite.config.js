f757180a1b7bb844b18ae0f44a538698f2ebdb72
Victor Meldgaard <49498101+meldg0d@users.noreply.github.com>
2025-03-21 12:57:34 +0100

    Deploy on github pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PokedexReact/', // This should match your repository name
+  base: '/PokedexReact', // This should match your repository name
   build: {
     // Ensure proper file extensions and MIME types
     rollupOptions: {
